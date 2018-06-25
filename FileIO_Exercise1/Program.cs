b70040217b1f81b1beed460412daa0a8d1062a6e
Kirstin Veltman <kirstin@veltman.co>
2018-06-25 14:14:24 -0700

    Add project files.

---
+++ FileIO_Exercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIO_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number: ");
+            string userText = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\derwy\source\repos\Text_Files\FileIO_Logs\log.txt", userText);
+
+            string repeatText = File.ReadAllText(@"C:\Users\derwy\source\repos\Text_Files\FileIO_Logs\log.txt");
+            Console.WriteLine("Here is your number: {0}", repeatText);
+            Console.ReadLine();
+
+        }
+    }
+}
