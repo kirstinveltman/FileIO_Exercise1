03c64f3f01a4a198452e6a5586982b28bb49b8c2
Kirstin Veltman <kirstin@veltman.co>
2018-06-25 14:49:07 -0700

    new exercise for page 106

--- FileIO_Exercise1/Program.cs
+++ FileIO_Exercise1/Program.cs
@@ -11,13 +11,25 @@ namespace FileIO_Exercise1
     {
         static void Main(string[] args)
         {
-
+            // page 104 exercises for logging
             Console.WriteLine("Please enter a number: ");
             string userText = Console.ReadLine();
             File.WriteAllText(@"C:\Users\derwy\source\repos\Text_Files\FileIO_Logs\log.txt", userText);
 
             string repeatText = File.ReadAllText(@"C:\Users\derwy\source\repos\Text_Files\FileIO_Logs\log.txt");
             Console.WriteLine("Here is your number: {0}", repeatText);
+            Console.ReadLine();
+
+
+            // page 106 exercises for DateTime
+            DateTime currentDateTime = DateTime.Now;
+            Console.WriteLine("The current date and time is: {0}", currentDateTime);
+
+            Console.WriteLine("\nPlease enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            DateTime newDateTime = currentDateTime.AddHours(userNumber);
+
+            Console.WriteLine("Your number ({0}) of hours from now is {1}.", userNumber, newDateTime);
             Console.ReadLine();
 
         }
